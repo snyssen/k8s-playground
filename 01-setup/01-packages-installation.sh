dae14da1b943715e675c1ce21eaf79d576d67bec
Simon Nyssen <dev@snyssen.be>
2023-11-15 14:26:45 +0100

    feat: install kubernetes components

--- 01-setup/01-packages-installation.sh
+++ 01-setup/01-packages-installation.sh
@@ -63,3 +63,39 @@ grep -B 12 "SystemdCgroup" /etc/containerd/config.toml
 
 # Restart containerd
 sudo systemctl restart containerd
+
+## Install Kubernetes packages - kubeadm, kubelet and kubectl
+## https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+#! WARN: The Pluralsight uses the packages from https://apt.kubernetes.io, but the official doc has marked this repo as deprecated and recommends using https://pkgs.k8s.io instead
+# I am thus following the official doc. Note that each Kubernetes version now have their own repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.26/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# Then add packages list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.26/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+# Check available packages
+apt-cache policy kubelet
+# Above should return something like:
+# kubelet:
+#   Installed: (none)
+#   Candidate: 1.26.10-1.1
+#   Version table:
+#      1.26.10-1.1 500
+#         500 https://pkgs.k8s.io/core:/stable:/v1.26/deb  Packages
+# [...]
+# Install kubelet, kubeadm and kubectl, and pin their version
+#* NOTE: no need to force install of a specific version since this is already taken care of by the specific apt repo
+sudo apt-get install -y kubelet kubeadm kubectl
+# Prevent automatic upgrades
+# imo this should not be necessary since the apt repo already pins the k8s to a minor one, but I guess it is added for maximum stability, to even prevent automatic patch updates
+sudo apt-mark hold kubelet kubeadm kubectl containerd
+
+# Check systemd units
+# Kubelet will report a status 1 failure. This is expected since it is waiting to be part of a k8s cluster
+systemctl status kubelet
+# containerd should be active
+systemctl status containerd
+
+# If they are inactive, start and enable them (so they start on reboot)
+sudo systemctl start kubelet && sudo systemctl enable kubelet
+sudo systemctl start containerd && sudo systemctl enable containerd
--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,25 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+unless Vagrant.has_plugin?("vagrant-disksize")
+  raise  Vagrant::Errors::VagrantError.new, "vagrant-disksize plugin is missing. Please install it using 'vagrant plugin install vagrant-disksize' and rerun 'vagrant up'"
+end
+
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/jammy64"
+  config.disksize.size = '70GB'
 
   config.hostmanager.enabled = true
   config.hostmanager.manage_host = true
   config.hostmanager.manage_guest = true
   config.hostmanager.ignore_private_ip = false
   config.hostmanager.include_offline = true
+
+  config.vm.provider "virtualbox" do |vb|
+    # Customize the amount of memory on the VM:
+    vb.memory = "4096"
+    vb.cpus = 2
+  end
 
   controlPlaneNodeName = "c1-cp1"
   config.vm.define "#{controlPlaneNodeName}" do |controlPlaneNode|
